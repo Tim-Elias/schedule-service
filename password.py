b203843c41a044c3f986da22c0cc16b44763a61c
Viktoria <v.s.shuvaeva@gmail.com>
2024-09-17 10:43:26 +0700

    fix

--- password.py
+++ password.py
@@ -1,10 +1,15 @@
 from db_manager import DatabaseManager
+import os
+from dotenv import load_dotenv
 
+
+
+load_dotenv()
 db = DatabaseManager()
 
 # Создаем таблицы
 db.create_tables()
 
-password='qweasdzxc'
-username='123'
+password=os.getenv('PASSWORD')
+username=os.getenv('LOGIN')
 db.add_user(username, password)
