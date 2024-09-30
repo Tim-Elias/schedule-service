b501de2955d0944bf5ea418664aa0fddcc3b81fd
Ilia Timofeev <ilya48007@gmail.com>
2024-09-30 12:16:15 +0700

    password

--- password.py
+++ password.py
@@ -10,6 +10,6 @@ db = UserManager()
 
 password=os.getenv('PASSWORD')
 username=os.getenv('LOGIN')
-#db.add_user_password(username, password)
-email='mintbunny.eng@gmail.com'
-db.add_user_google(email, auth_type='google')
+db.add_user_password(username, password)
+#email='mintbunny.eng@gmail.com'
+#db.add_user_google(email, auth_type='google')
