dec02a62e47c7f267ee2d37b18c9556c84a35785
Ilia Timofeev <ilya48007@gmail.com>
2024-09-30 12:07:11 +0700

    migrations

--- app/database/migrations/migration-001.py
+++ app/database/migrations/migration-001.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine, Column, Time
+from sqlalchemy import create_engine, Column, Time, inspect, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import text
 import os
 from dotenv import load_dotenv
 
@@ -14,24 +13,28 @@ def run_migration():
     # Подключение к базе данных
     DATABASE_URL = os.getenv('DATABASE_URL')
     engine = create_engine(DATABASE_URL, echo=True)
-    connection = engine.connect()
 
-    # Проверяем, существует ли столбец
-    from sqlalchemy import inspect
     inspector = inspect(engine)
     columns = [col['name'] for col in inspector.get_columns('schedule')]
 
     if 'time_of_day' not in columns:
-        # Выполняем SQL-команду для добавления столбца
         try:
-            connection.execute(text('ALTER TABLE schedule ADD COLUMN time_of_day TIME;'))
-            print('Столбец time_of_day успешно добавлен.')
+            with engine.connect() as connection:
+                # Начинаем транзакцию
+                trans = connection.begin()
+                try:
+                    connection.execute(text('ALTER TABLE schedule ADD COLUMN time_of_day TIME;'))
+                    # Фиксируем транзакцию
+                    trans.commit()
+                    print('Столбец time_of_day успешно добавлен.')
+                except Exception as e:
+                    # Откатываем транзакцию в случае ошибки
+                    trans.rollback()
+                    print(f'Ошибка при добавлении столбца: {e}')
         except Exception as e:
-            print(f'Ошибка при добавлении столбца: {e}')
+            print(f'Ошибка при подключении к базе данных: {e}')
     else:
         print('Столбец time_of_day уже существует.')
-
-    connection.close()
 
 if __name__ == '__main__':
     run_migration()
