58540ef416bbef4d9a52355c3f4ad36b469f068b
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-04 13:35:15 +0700

    fix_alembic

--- app/database/migrations/migration.py
+++ app/database/migrations/migration.py
@@ -1,12 +1,12 @@
-from password import admin
-from migration_schedule import run_migration_schedule
-from migration_users import run_migration_users
+from app.database.migrations.password import admin
+#from app.database.migrations.migration_schedule import run_migration_schedule
+#from app.database.migrations.migration_users import run_migration_users
 
 
 def migrate(app):
     password=app.config['PASSWORD']
     login=app.config['LOGIN']
     admin(password, login)
-    run_migration_schedule()
-    run_migration_users()
+    #run_migration_schedule()
+    #run_migration_users()
     
