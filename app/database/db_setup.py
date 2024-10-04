f768e7493565f06d94ad0a51075c0a562cabcfb6
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-04 13:42:13 +0700

    fix

--- app/database/db_setup.py
+++ app/database/db_setup.py
@@ -6,6 +6,7 @@ Base = declarative_base()
 
 def init_db(app):
     DATABASE_URL = app.config['SQLALCHEMY_DATABASE_URI']
+    from app.models import RequestLog, User, Schedule
     engine = create_engine(DATABASE_URL, echo=False)
     Session = sessionmaker(bind=engine)
     # Создание всех таблиц
