1a5b14a7b8669cfe36916a541b56ed189138e845
Ilia Timofeev <ilya48007@gmail.com>
2024-10-04 17:39:44 +0700

    html

--- app/static/js/active_schedules.js
+++ app/static/js/active_schedules.js
@@ -156,7 +156,7 @@ function loadSchedulesActive() {
                     }</p>
                     <p class="card-text"><strong>Schedule Type:</strong> ${scheduleType}</p>
                     <p class="card-text"><strong>Last Run:</strong> ${
-                      schedule.lastRun || "Never"
+                      schedule.last_run || "Never"
                     }</p>
                     <button class="btn btn-danger btn-sm" onclick="deactivateScheduleActive(${
                       schedule.id
