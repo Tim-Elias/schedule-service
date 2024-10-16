b8f40d31540f92a44f1aa8ace6e3f6557651132b
Ilia Timofeev <ilya48007@gmail.com>
2024-10-16 15:53:24 +0700

    cors

--- app/routes/schedules_routes.py
+++ app/routes/schedules_routes.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restx import Namespace, Resource, fields
-from flask_jwt_extended import jwt_required
+# from flask_jwt_extended import jwt_required
 import logging
 from flask_cors import cross_origin
 
@@ -41,7 +41,7 @@ get_schedule_model = schedules_ns.model('GetSchedules', {
 @schedules_ns.route('/')
 class AllSchedulesResource(Resource):
     @cross_origin()
-    @jwt_required()
+    # @jwt_required()
     @schedules_ns.expect(get_schedule_model)
     @schedules_ns.marshal_list_with(schedule_response_model)
     def get(self):
